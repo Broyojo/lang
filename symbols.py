91256bafc113937137920e9ce9220bd4b99c83dd
Broyojo <dha@xoba.com>
2021-06-23 22:54:02 -0400

    added symbols

---
+++ symbols.py
@@ -0,0 +1,57 @@
+# keywords
+func
+struct
+enum
+let
+return
+if
+elif
+else
+for
+nil
+match
+switch
+
+
+
+# symbols
++   # plus
+-   # minus
+*   # multiply
+/   # divide
+%   # modulo
+**  # exponentiation
++=  # plus-equals
+-=  # minus-equals
+*=  # multiply-equals
+/=  # divide-equals
+**= # exponentiation-equals
+~   # bit-wise not
+&   # bit-wise and
+|   # bit-wise or
+^   # bit-wise xor
+>>  # shift right
+<<  # shift left
+~=  # bit-wise-not-equals
+&=  # bit-wise-and-equals
+|=  # bit-wise-or-equals
+^=  # bit-wise-xor-equals
+>>= # shift-right-equals
+<<= # shift-left-equals
+!   # logical not
+&&  # logical and
+||  # logical or
+!=  # not-equals
+==  # equality test
+<   # less than
+>   # greater than
+<=  # less than or equal to
+>=  # greater than or equal to
+<=> # 3 way comparison
+@   # reference pointer
+$   # dereference pointer
+?   # nil-abble container (Option enum)
+#   # comment
+""  # string
+''  # char
+\   # special characters i.e. \n
